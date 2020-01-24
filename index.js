75f36cafd590e9be428e4e6a8681e19f77a757e6
AzodFR <40648473+AzodFR@users.noreply.github.com>
2020-01-24 23:01:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+'use strict';
+bot.on('ready', function(){
+    bot.user.setActivity('BreakerLand !')
+    console.log('Connected');
+})
+
+bot.on('message', function(message){
+   if (message.channel.id === "614477179853733918"){
+     if (message.content === "@Toryar"){
+        message.channel.send("Tiens ton serveur !")
+     }
+ }
 
 
+
+});
+
+
+
+bot.login(process.env.BOT_TOKEN)
