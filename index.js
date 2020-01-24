7dfa50aae174234a28ef5c5c8bfc202974fa122b
AzodFR <40648473+AzodFR@users.noreply.github.com>
2020-01-24 23:15:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 'use strict';
 bot.on('ready', function(){
-    bot.user.setActivity('Tenez BreakerLand !')
+    bot.user.setActivity('BreakerLand !')
     console.log('Connected');
 })
 
 bot.on('message', function(message){
-     if (message.isMemberMentioned(MessageMentions.users.id === '351333156403675137')){
+   if (message.channel.id === "614477179853733918"){
+     if (message.content === "/toryar"){
         message.channel.send("Tiens ton serveur !")
      }
-
+ }
 
 
 
