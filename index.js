06c67ade96604e1f731be4bdc6e0ed007714011f
AzodFR <40648473+AzodFR@users.noreply.github.com>
2020-01-24 23:25:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ bot.on('ready', function(){
 
 bot.on('message', function(message){
      if (message.content === "/toryar"){
-        message.channel.send("Tiens ton serveur !")
+        message.channel.send("Tiens ton serveur ! <@351333156403675137>")
      }
  
 
