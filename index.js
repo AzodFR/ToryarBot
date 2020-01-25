1b175ed6dffff2e8735a50484d955af84c8a0631
AzodFR <40648473+AzodFR@users.noreply.github.com>
2020-01-25 02:05:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ bot.on('message', function(message){
      if (message.content === "/toryar"){
         message.delete()
         message.channel.send("Tiens ton serveur ! <@351333156403675137>")
-        console.log('Toryar ' message.author.username);
+        console.log('Toryar '+ message.author.username);
      }
     else if (message.content === "/coco"){
         message.delete()
         message.channel.send("Tiens ton tps ! <@192635336462041089>")
-        console.log('Coco ' message.author.username);
+        console.log('Coco '+ message.author.username);
      }
     else if (message.content === "/azod"){
         message.delete()
         message.channel.send("Tiens ton bot ! <@209392112914006016>")
-        console.log('Azod ' message.author.username);
+        console.log('Azod '+ message.author.username);
      }
  
 
