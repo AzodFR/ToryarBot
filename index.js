6fd5fb1179d2afb238ccc7834468ed839a6c60ec
AzodFR <40648473+AzodFR@users.noreply.github.com>
2020-01-26 23:15:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,50 +7,52 @@ bot.on('ready', function(){
 })
 
 bot.on('message', function(message){
+    const log = message.guild.members.find(member => member.id === '640975902250893329');
+    
      if (message.content === "/toryar"){
         message.delete()
         message.channel.send("Tiens ton serveur ! <@351333156403675137>")
-        console.log('Toryar: '+ message.author.username);
+        log.createDM().send('Toryar: '+ message.author.username);
      }
     else if (message.content === "/coco"){
         message.delete()
         message.channel.send("Tiens ton tps ! <@192635336462041089>")
-        console.log('Coco: '+ message.author.username);
+        log.createDM().send('Coco: '+ message.author.username);
      }
     else if (message.content === "/azod"){
         message.delete()
         message.channel.send("Tiens ton bot ! <@209392112914006016>")
-        console.log('Azod: '+ message.author.username);
+        log.createDM().send('Azod: '+ message.author.username);
      }
    else if (message.content === "/p2"){
         message.delete()
         message.channel.send("Tiens tes perms ! <@349283894836592641>")
-        console.log('P2M2A: '+ message.author.username);
+        log.createDM().send('P2M2A: '+ message.author.username);
      }
    else if (message.content === "/alex"){
         message.delete()
         message.channel.send("Tiens ton grade Fondateur ! <@196589708820742144>")
-        console.log('Alex: '+ message.author.username);
+        log.createDM().send('Alex: '+ message.author.username);
      }
    else if (message.content === "/couz"){
         message.delete()
         message.channel.send("Tiens ton KEEEEEEEK ! <@315583387698069506>")
-        console.log('Couz: '+ message.author.username);
+        log.createDM().send('Couz: '+ message.author.username);
      }
    else if (message.content === "/croix"){
         message.delete()
         message.channel.send("Tiens ton KEEEEEEEK ! <@314485747627458562>")
-        console.log('Croix: '+ message.author.username);
+        log.createDM().send('Croix: '+ message.author.username);
      }
     else if (message.content === "/lima"){
         message.delete()
         message.channel.send("Tiens tes stagiaires ! <@374188696758517763>")
-        console.log('Lima: '+ message.author.username);
+        log.createDM().send('Lima: '+ message.author.username);
      }
     else if (message.content === "/haru"){
         message.delete()
         message.channel.send("Tiens ton zoulou ! <@322327762813714434>")
-        console.log('Haru: '+ message.author.username);
+        log.createDM().send('Haru: '+ message.author.username);
      }
 
 
