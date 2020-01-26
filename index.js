604f28c53867962d5823f48e6b688e80d64a9d13
AzodFR <40648473+AzodFR@users.noreply.github.com>
2020-01-26 23:57:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,47 +13,47 @@ bot.on('message', function(message){
      if (message.content === "/toryar"){
         message.delete()
         message.channel.send("Tiens ton serveur ! <@351333156403675137>")
-        log.send('Toryar: '+ message.author.username+' '+date);
+        log.send('Toryar: '+ message.author.username+' le '+date.prototype.getDate()+'/'+date.prototype.getMonth()+'/'+date.prototype.getFullYear()+' à '+date.prototype.getTime());
      }
     else if (message.content === "/coco"){
         message.delete()
         message.channel.send("Tiens ton tps ! <@192635336462041089>")
-        log.send('Coco: '+ message.author.username+' '+date);
+        log.send('Coco: '+ message.author.username+' '+' le '+date.prototype.getDate()+'/'+date.prototype.getMonth()+'/'+date.prototype.getFullYear()+' à '+date.prototype.getTime());
      }
     else if (message.content === "/azod"){
         message.delete()
         message.channel.send("Tiens ton bot ! <@209392112914006016>")
-        log.send('Azod: '+ message.author.username+' '+date);
+        log.send('Azod: '+ message.author.username+' '+' le '+date.prototype.getDate()+'/'+date.prototype.getMonth()+'/'+date.prototype.getFullYear()+' à '+date.prototype.getTime());
      }
    else if (message.content === "/p2"){
         message.delete()
         message.channel.send("Tiens tes perms ! <@349283894836592641>")
-        log.send('P2M2A: '+ message.author.username+' '+date);
+        log.send('P2M2A: '+ message.author.username+' '+' le '+date.prototype.getDate()+'/'+date.prototype.getMonth()+'/'+date.prototype.getFullYear()+' à '+date.prototype.getTime());
      }
    else if (message.content === "/alex"){
         message.delete()
         message.channel.send("Tiens ton grade Fondateur ! <@196589708820742144>")
-        log.send('Alex: '+ message.author.username+' '+date);
+        log.send('Alex: '+ message.author.username+' '+' le '+date.prototype.getDate()+'/'+date.prototype.getMonth()+'/'+date.prototype.getFullYear()+' à '+date.prototype.getTime());
      }
    else if (message.content === "/couz"){
         message.delete()
         message.channel.send("Tiens ton KEEEEEEEK ! <@315583387698069506>")
-        log.send('Couz: '+ message.author.username+' '+date);
+        log.send('Couz: '+ message.author.username+' '+' le '+date.prototype.getDate()+'/'+date.prototype.getMonth()+'/'+date.prototype.getFullYear()+' à '+date.prototype.getTime());
      }
    else if (message.content === "/croix"){
         message.delete()
         message.channel.send("Tiens ton KEEEEEEEK ! <@314485747627458562>")
-        log.send('Croix: '+ message.author.username+' '+date);
+        log.send('Croix: '+ message.author.username+' '+' le '+date.prototype.getDate()+'/'+date.prototype.getMonth()+'/'+date.prototype.getFullYear()+' à '+date.prototype.getTime());
      }
     else if (message.content === "/lima"){
         message.delete()
         message.channel.send("Tiens tes stagiaires ! <@374188696758517763>")
-        log.send('Lima: '+ message.author.username+' '+date);
+        log.send('Lima: '+ message.author.username+' '+' le '+date.prototype.getDate()+'/'+date.prototype.getMonth()+'/'+date.prototype.getFullYear()+' à '+date.prototype.getTime());
      }
     else if (message.content === "/haru"){
         message.delete()
         message.channel.send("Tiens ton zoulou ! <@322327762813714434>")
-        log.send('Haru: '+ message.author.username+' '+date);
+        log.send('Haru: '+ message.author.username+' '+' le '+date.prototype.getDate()+'/'+date.prototype.getMonth()+'/'+date.prototype.getFullYear()+' à '+date.prototype.getTime());
      }
     }
 
