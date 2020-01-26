c4444227726248a5be462a8aeb21620615b70d5d
AzodFR <40648473+AzodFR@users.noreply.github.com>
2020-01-27 00:15:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,53 +13,52 @@ bot.on('message', function(message){
     var jour = date.getDate();
     var mois = date.getMonth()+1;
     var annee = date.getFullYear();
-    var heure = date.getHours();
-    var minute = date.getMinutes()
-    var seconde = date.getSeconds()
+    var heure = date.toTimeString().replace('GMT+0000 (Coordinated Universal Time)', '');
+    var msg = +' '+' le '+jour+'/'+mois+'/'+annee+' à '+heure;
      if (message.content === "/toryar"){
         message.delete()
         message.channel.send("Tiens ton serveur ! <@351333156403675137>")
-        log.send('Toryar: '+ message.author.username+' le '+date.prototype.getDate()+'/'+date.prototype.getMonth()+'/'+date.prototype.getFullYear()+' à '+date.prototype.getTime());
+        log.send('Toryar: '+ message.author.username+msg);
      }
     else if (message.content === "/coco"){
         message.delete()
         message.channel.send("Tiens ton tps ! <@192635336462041089>")
-        log.send('Coco: '+ message.author.username+' '+' le '+date.prototype.getDate()+'/'+date.prototype.getMonth()+'/'+date.prototype.getFullYear()+' à '+date.prototype.getTime());
+        log.send('Coco: '+ message.author.username+msg);
      }
     else if (message.content === "/azod"){
         message.delete()
         message.channel.send("Tiens tes conventions ! <@209392112914006016>")
-        log.send('Azod: '+ message.author.username+' '+' le '+jour+'/'+mois+'/'+annee+' à '+heure+':'+minute+':'+seconde+' '+date.toTimeString());
+        log.send('Azod: '+ message.author.username+msg);
      }
    else if (message.content === "/p2"){
         message.delete()
         message.channel.send("Tiens tes perms ! <@349283894836592641>")
-        log.send('P2M2A: '+ message.author.username+' '+' le '+date.prototype.getDate()+'/'+date.prototype.getMonth()+'/'+date.prototype.getFullYear()+' à '+date.prototype.getTime());
+        log.send('P2M2A: '+ message.author.username+msg);
      }
    else if (message.content === "/alex"){
         message.delete()
         message.channel.send("Tiens ton grade Fondateur ! <@196589708820742144>")
-        log.send('Alex: '+ message.author.username+' '+' le '+date.prototype.getDate()+'/'+date.prototype.getMonth()+'/'+date.prototype.getFullYear()+' à '+date.prototype.getTime());
+        log.send('Alex: '+ message.author.username+msg);
      }
    else if (message.content === "/couz"){
         message.delete()
         message.channel.send("Tiens ton KEEEEEEEK ! <@315583387698069506>")
-        log.send('Couz: '+ message.author.username+' '+' le '+date.prototype.getDate()+'/'+date.prototype.getMonth()+'/'+date.prototype.getFullYear()+' à '+date.prototype.getTime());
+        log.send('Couz: '+ message.author.username+msg);
      }
    else if (message.content === "/croix"){
         message.delete()
         message.channel.send("Tiens ton KEEEEEEEK ! <@314485747627458562>")
-        log.send('Croix: '+ message.author.username+' '+' le '+date.prototype.getDate()+'/'+date.prototype.getMonth()+'/'+date.prototype.getFullYear()+' à '+date.prototype.getTime());
+        log.send('Croix: '+ message.author.username+msg);
      }
     else if (message.content === "/lima"){
         message.delete()
         message.channel.send("Tiens tes stagiaires ! <@374188696758517763>")
-        log.send('Lima: '+ message.author.username+' '+' le '+date.prototype.getDate()+'/'+date.prototype.getMonth()+'/'+date.prototype.getFullYear()+' à '+date.prototype.getTime());
+        log.send('Lima: '+ message.author.username+msg);
      }
     else if (message.content === "/haru"){
         message.delete()
         message.channel.send("Tiens ton zoulou ! <@322327762813714434>")
-        log.send('Haru: '+ message.author.username+' '+' le '+date.prototype.getDate()+'/'+date.prototype.getMonth()+'/'+date.prototype.getFullYear()+' à '+date.prototype.getTime());
+        log.send('Haru: '+ message.author.username+msg);
      }
     }
 
