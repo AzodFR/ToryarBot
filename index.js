c9234868ec5ab3823360bf5c4b110b4a7ad839b6
AzodFR <40648473+AzodFR@users.noreply.github.com>
2020-01-29 00:01:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -139,21 +139,15 @@ bot.on('message', function(message){
             log.send('Clem: '+ message.author.username+msg);
         }
     else if (message.content === "/notif"){
-        if(message.member.nickname === null){
-            var nom = message.member.user.username
-            }
-        else{
-            var nom = message.member.nickname
-            }
             if(!(noar.has(message.member.id))){
               noar.set(message.member.id);
               message.delete()
-              message.channel.send("Vous ne serez plus notifié par l'association" + nom)
+              message.channel.send("Vous ne serez plus notifié par l'association " + message.member.user.username)
          }
             else{
                 noar.delete(message.member.id);
                 message.delete()
-                message.channel.send("Vous êtes de nouveau notifiable " + nom)
+                message.channel.send("Vous êtes de nouveau notifiable " + message.member.user.username)
             }
     }
 
